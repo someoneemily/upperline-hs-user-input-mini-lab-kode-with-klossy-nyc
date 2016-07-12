85db4cdbc7f1d7caa8cb71ce27693a066d535a6d
Emily C <iemily.cwx@gmail.com>
2016-07-12 03:02:18 +0000

    Done.

--- ruby/trip.rb
+++ ruby/trip.rb
@@ -1,1 +1,10 @@
 #Code here!
+puts "Where would you like to stay?"
+location = gets.chomp
+puts "What sites do you want to visit?"
+sites = gets.chomp
+puts "What food do you want to eat?"
+food = gets.chomp
+puts "How many nights do you want to stay?"
+nights = gets.chomp
+puts "You want to stay at #{location} when you visit #{sites}. You really want to try out #{food} and are staying #{nights} nights to do so."
